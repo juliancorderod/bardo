4e4098db8e1a2045c93e45ae487a440c450c6fa3
Unknown <jc6984@nyu.edu>
2018-06-28 18:28:32 -0500

    jugando con la ciudad

--- Assets/scripts/CityBuilder.cs
+++ Assets/scripts/CityBuilder.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+
 public class CityBuilder : MonoBehaviour
 {
 
     public GameObject building;
     public float maxBuildingsSide;
     float spaceBetweenBuildingsX, spaceBetweenBuildingsY;
+
+    float sizeX, sizeZ;
+    float accumulatedPositionX, accumulatedPositionZ;
 
     // Use this for initialization
     void Start()
@@ -17,24 +23,55 @@ public class CityBuilder : MonoBehaviour
         {
             for (int y = 0; y < maxBuildingsSide; y++)
             {
-                if (x % 4 == 0)
-                    spaceBetweenBuildingsX = 1;
-                else
-                    spaceBetweenBuildingsX = 0;
+                //if (x % 4 == 0)
+                //    spaceBetweenBuildingsX = 1;
+                //else
+                //    spaceBetweenBuildingsX = 0;
 
-                if (y % 3 == 0)
-                    spaceBetweenBuildingsY = 2;
-                else
-                    spaceBetweenBuildingsY = 0;
+                //if (y % 3 == 0)
+                //    spaceBetweenBuildingsY = 2;
+                //else
+                //spaceBetweenBuildingsY = 0;
 
 
-                GameObject g = Instantiate(building, new Vector3(x * (10f + spaceBetweenBuildingsX), 0, y * (10f + spaceBetweenBuildingsY)), Quaternion.identity);
 
-                g.transform.localScale = new Vector3(10, Random.Range(4.0f, 20.0f), 10);
+                sizeZ = Random.Range(8.0f, 12.0f);
+                sizeX = Random.Range(8.0f, 12.0f);
+                accumulatedPositionZ += sizeZ;
+
+
+
+                Debug.Log(spaceBetweenBuildingsX);
+                GameObject g = Instantiate(building, transform);
+                g.transform.localPosition = new Vector3(x * 10, -5, y * 10);
+
+
+
+                g.transform.localScale = new Vector3(sizeX, Random.Range(8.0f, 25.0f) * (x / maxBuildingsSide) * 3 + 0.5f, sizeZ);
+
+                //if (g.transform.localPosition.x < 150 && g.transform.localPosition.z < 120)
+                //  g.transform.localScale = new Vector3(g.transform.localScale.x, g.transform.localScale.y * 4, g.transform.localScale.z);
+
+
+                g.transform.localPosition -= new Vector3((maxBuildingsSide * 10) / 2, 0, (maxBuildingsSide * 10) / 2);//esto les centra
+
+                if (x % 5 == 0)
+                    Destroy(g);
+
+
+                if (y % 4 == 0)
+                    Destroy(g);
+
+
 
             }
+            accumulatedPositionZ = 0;
         }
+
+
     }
+
+
 
     // Update is called once per frame
     void Update()
---
+++ Assets/scripts/audioSpectrum/PersonDance.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonDance : MonoBehaviour
+{
+
+    public AudioSpectrum spectrum;
+
+    public Transform[] joints;
+
+    public float levelScale, initScale;
+
+    public bool visualizer;
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //can only have as many joints as bands (10)!
+        //if (visualizer)
+        //{
+        for (int i = 0; i < joints.Length; i++)
+        {
+            joints[i].localScale = Vector3.one * (levelScale * spectrum.MeanLevels[i] + initScale);
+            //            Debug.Log(i + "|||" + spectrum.MeanLevels[i]);
+        }
+        //}
+
+
+
+
+    }
+}
