ec09ca79f382894d34f7ef34827498037b364ec2
Unknown <jc6984@nyu.edu>
2018-07-16 00:33:19 -0500

    stars in and starting cig smoke

--- Assets/scripts/MuerteSubitaManager.cs
+++ Assets/scripts/MuerteSubitaManager.cs
@@ -9,8 +9,13 @@ public class MuerteSubitaManager : MonoBehaviour
 
     MeshRenderer[] preLights;
     List<MeshRenderer> lights = new List<MeshRenderer>();
+    List<GameObject> animationList = new List<GameObject>();
 
-    public float levelScale, sphereRadius;
+    [HideInInspector]
+    public List<GameObject> stars = new List<GameObject>();
+
+
+    public float lightsLevelScale, starsLevelScale, sphereRadius;
 
     public LayerMask windowsLayer;
 
@@ -22,7 +27,7 @@ public class MuerteSubitaManager : MonoBehaviour
 
     float shortDelay;
 
-    public GameObject kasWalkAnim;
+    public GameObject kasWalkAnim, jBeer1;
 
     float originalHue;
 
@@ -31,6 +36,8 @@ public class MuerteSubitaManager : MonoBehaviour
     RaycastHit hit;
 
     Vector3 rayPosAdjustment;
+
+    bool animsActive;
 
     // Use this for initialization
     void Start()
@@ -45,7 +52,7 @@ public class MuerteSubitaManager : MonoBehaviour
         Collider[] windows = Physics.OverlapSphere(new Vector3(25, 30, 9), sphereRadius, windowsLayer);
 
         preLights = new MeshRenderer[windows.Length];
-        Debug.Log(windows.Length);
+        //Debug.Log(windows.Length);
 
         for (int i = 0; i < windows.Length; i++)
         {
@@ -85,29 +92,56 @@ public class MuerteSubitaManager : MonoBehaviour
                 }
             }
 
-            Debug.Log(lights.Count);
+            // Debug.Log(lights.Count);
 
 
             for (int i = 0; i < lights.Count; i++)
             {
+                bool placedAnim = false;
+                //lights[i].material.color = Color.red;
 
-                lights[i].material.color = Color.red;
-
-
-                if (lights[i].name == "pCube44" || lights[i].name == "pCube43")
+                if (!placedAnim)
                 {
-                    if (Random.Range(0, 10) > 8)
+                    if (lights[i].name == "pCube44" || lights[i].name == "pCube43")
                     {
-                        GameObject kwa = Instantiate(kasWalkAnim, lights[i].transform);
+                        if (Random.Range(0, 10) > 8)
+                        {
+                            GameObject kwa = Instantiate(kasWalkAnim, lights[i].transform);
 
-                        kwa.transform.localPosition = new Vector3(0, 0f, -4.35f);
-                        Debug.Log("kwa");
+                            kwa.transform.localPosition = new Vector3(0, 0f, -4.35f);
+                            animationList.Add(kwa);
+                            placedAnim = true;
+                        }
                     }
                 }
+                if (!placedAnim)
+                {
+                    if (lights[i].name == "pCube45" || lights[i].name == "pCube43")
+                    {
+                        if (lights[i].transform.position.y < 40 && lights[i].transform.position.x < 100)
+                        {
+                            if (Random.Range(0, 10) > 5)
+                            {
+                                GameObject jb1 = Instantiate(jBeer1, lights[i].transform);
 
+                                jb1.transform.localPosition = new Vector3(-4.15f, 0f, -4.4f);
 
+                                jb1.GetComponent<Animator>().speed = Random.Range(0.8f, 1.2f);
+                                animationList.Add(jb1);
+                                placedAnim = true;
+                            }
+                        }
+                    }
+                }
             }
 
+            for (int i = 0; i < animationList.Count; i++)
+            {
+                animationList[i].SetActive(false);
+            }
+            animsActive = false;
+
+            Debug.Log(animationList.Count);
             haveReOrdered = true;
             this.enabled = false;
         }
@@ -126,17 +160,32 @@ public class MuerteSubitaManager : MonoBehaviour
 
                 }
             }
-            else
+            else//actually start messing with stuff here!
             {
+                if (!animsActive)
+                {
+                    for (int i = 0; i < animationList.Count; i++)
+                    {
+                        animationList[i].SetActive(true);
+                    }
+                    animsActive = true;
+                }
                 for (int i = 0; i < lights.Count; i++)
                 {
 
                     //lights[i].material.color = Color.HSVToRGB(0, 0, spectrum.MeanLevels[i % 10] * levelScale);
-                    lights[i].material.color = new Color(1, 1, 1, spectrum.MeanLevels[i % 10] * levelScale);
+                    lights[i].material.color = new Color(1, 1, 1, spectrum.MeanLevels[i % 10] * lightsLevelScale);
                     //lights[i].intensity = spectrum.MeanLevels[i % 10] * levelScale;
 
                     //            Debug.Log(i + "|||" + spectrum.MeanLevels[i]);
                 }
+
+                for (int i = 0; i < stars.Count; i++)
+                {
+                    stars[i].transform.localScale = Vector3.one * (1 + (spectrum.MeanLevels[(i % 2) + 8] * starsLevelScale));
+                    //the (i % 3) + 7 always returns 6,7,8 in this case
+                }
+
             }
 
             if (lightHue < 0)
---
+++ Assets/scripts/starManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starManager : MonoBehaviour
+{
+
+    public GameObject starObj;
+    public float starRange, starNum, maxSize;
+    public MuerteSubitaManager ms;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+        for (int i = 0; i < starNum; i++)
+        {
+            Vector3 randOnSphere = Random.onUnitSphere * starRange;
+            if (randOnSphere.y > 0)
+            {
+                GameObject s = Instantiate(starObj, randOnSphere, Quaternion.identity);
+                s.transform.localScale = Vector3.one * Random.Range(0.5f, maxSize);
+                s.transform.parent = transform;
+
+                ms.stars.Add(s);
+            }
+            else
+                i--;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
